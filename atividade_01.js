db4c3f3bd9d99cf9e2c0754cede35909948310bc
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2024-12-29 13:22:12 -0300

    necessary corrections made to the codes

--- atividade_01.js
+++ atividade_01.js
@@ -1,6 +1,2 @@
-document.addEventListener("DOMContentLoaded" , function(){
-    alert("DOM fully loaded and parsed");
-    
-    const text = prompt("Type a sentence:")
-    alert(text.length)
-})
+const text = prompt("Type a sentence:")
+alert(text.length)
--- atividade_02.js
+++ atividade_02.js
@@ -1,13 +1,9 @@
-document.addEventListener("DOMContentLoaded" , function(){
-    alert("DOM fully loaded and parsed");
-    
-    const num1 = prompt("Enter a number:")
-    const num2 = prompt("Enter another number:")
+const num1 = parseFloat(prompt("Enter a number:"))
+const num2 = parseFloat(prompt("Enter another number:"))
 
-    const sum = num1 + num2
-    const subtraction = num1 - num2
-    const product = num1 * num2
-    const quotient = num1 / num2
+const sum = num1 + num2
+const subtraction = num1 - num2
+const product = num1 * num2
+const quotient = num1 / num2
 
-    alert(` Results:\n Sum: ${sum}\n Subtraction: ${subtraction}\n Multiplication: ${product}\n Division: ${quotient.toFixed(2)}`)
-})
+alert(` Results:\n Sum: ${sum}\n Subtraction: ${subtraction}\n Multiplication: ${product}\n Division: ${quotient.toFixed(2)}`)
